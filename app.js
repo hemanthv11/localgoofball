559e6fc13b9a031282ae6bf7e85b4b112cccbe95
Hemanth Vemulapalli <hv85856@gmail.com>
2023-04-06 00:05:02 +0530

    gatway changes (initial)

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import { InteractionType, InteractionResponseType } from 'discord-interactions'
+import WebSocket from 'ws'
 import {VerifyDiscordRequest} from './utils/requests.js'
 import express from 'express'
 import dotenv from 'dotenv'
@@ -15,16 +16,16 @@ app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }))
 app.post('/interactions', async function (req, res) {
 	const { type, id, data, token, member } = req.body;
     if (type === InteractionType.PING) {
-    	return res.send({ type: InteractionResponseType.PONG });
+    	return res.send({ type: InteractionResponseType.PONG })
     }
 
     if (type === InteractionType.APPLICATION_COMMAND) {
-		console.log(data);
+		console.log(data)
 		if(data.name === 'sayhi') {
 			const Interaction = req.body;
 			let userid = Interaction.member.user.id;
 			let username = Interaction.member.user.username;
-			let response = 'Hello ' + username + '!';
+			let response = 'Hello ' + username + '!'
 			return res.send({
 				type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
 				data: {
